eb2b2a82274e8d54f142d82fa2d62c83d82af7d1
Vasyl Vasiuk <vasyl.vasiuk@moonshiner.at>
2024-03-16 21:18:07 +0100

    feat: refactor

--- components/MainSection.tsx
+++ components/MainSection.tsx
@@ -1,12 +1,9 @@
-import React, { FC, useEffect, useRef } from 'react'
+import React, { FC } from 'react'
 import { sliderVariants, transition } from "@/utils/framerMotion"
 import { motion, AnimatePresence } from "framer-motion"
 import styled from 'styled-components'
 import { Indicators } from '@/components';
 import { data } from "@/utils/data"
-import { nanoid } from 'nanoid'
-import { findOverflowLetters } from '@/utils/findOverflowLetters';
-
 interface MainSectionProps {
   direction: number;
   imageCount: number;
@@ -27,9 +24,6 @@ interface DragInfo {
 
 const MainSection: FC<MainSectionProps> = ({ direction, imageCount, activeImage, activeImageIndex, swipeToImage }) => {
   const { headline, currentImage } = activeImage;
-  const headingRef = useRef(null)
-  const wrapperRef = useRef(null)
-  const transformedHealine = headline.split('')
 
   const dragEndHandler = (dragInfo: DragInfo) => {
     const swipeThreshold = 50
@@ -37,16 +31,12 @@ const MainSection: FC<MainSectionProps> = ({ direction, imageCount, activeImage,
     if (dragInfo.offset.x < -swipeThreshold) return swipeToImage(1);
   }
 
-  useEffect(() => {
-    findOverflowLetters(wrapperRef.current!, headingRef.current!)
-  }, [activeImage]);
-
   return (
     <AnimatePresence>
+      <BackHeading>{headline}</BackHeading>
       <MainImageWrapper>
         <MainImage
           key={imageCount}
-          ref={wrapperRef}
           style={{ backgroundImage: `url(${currentImage})` }}
           custom={direction}
           variants={sliderVariants}
@@ -58,11 +48,8 @@ const MainSection: FC<MainSectionProps> = ({ direction, imageCount, activeImage,
           dragConstraints={{ left: 0, right: 0 }}
           onDragEnd={(_, dragInfo) => dragEndHandler(dragInfo)}
         >
-          <ContentWrapper>
-            <Heading ref={headingRef}> {transformedHealine.map(letter => <span key={nanoid()}>{letter}</span>)}
-            </Heading>
-            <Indicators data={data} activeImage={activeImageIndex} />
-          </ContentWrapper>
+          <Heading>{headline}</Heading>
+          <Indicators data={data} activeImage={activeImageIndex} />
         </MainImage>
       </MainImageWrapper>
     </AnimatePresence>
@@ -70,18 +57,17 @@ const MainSection: FC<MainSectionProps> = ({ direction, imageCount, activeImage,
 }
 
 const MainImageWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 100%;
+  position: relative;
+  height: 680px;
+  width: 512px;
   overflow: hidden;
 `
 const MainImage = styled(motion.div)`
-  height: 680px;
-  width: 512px;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  will-change: transform, opacity;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  padding-bottom: 100px;
+  height: 100%;
   border-radius: 10px;
   border: 1px solid #000;
   &:hover {
@@ -92,17 +78,22 @@ const MainImage = styled(motion.div)`
     cursor: grabbing;
   }
 `
-const ContentWrapper = styled.div`
+const Heading = styled.h1`
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-`
-const Heading = styled.h1`
   font-size: 220px;
   font-weight: 400;
   text-transform: uppercase;
   text-align: center;
+  user-select: none;
   width: 845px;
+`
+const BackHeading = styled(Heading)`
+  -webkit-text-fill-color: transparent; 
+  -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-color: #fff;
+  z-index: -1
 `
 export default MainSection;
